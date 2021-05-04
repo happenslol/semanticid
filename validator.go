42542af885cb3611079861e477f0badce7cdd259
Hilmar Wiegand <me@hwgnd.de>
2021-05-04 19:47:34 +0200

    Use custom type funcs and validator tests

--- validator.go
+++ validator.go
@@ -1,35 +1,65 @@
 package semanticid
 
-import "github.com/go-playground/validator/v10"
+import (
+	"fmt"
+	"reflect"
+	"strings"
 
-func SemanticIDCollectionValidation(fl validator.FieldLevel) bool {
+	"github.com/go-playground/validator/v10"
+)
+
+func validateSIDPrefix(sid SemanticID, namespace, collection string) bool {
+	if namespace != "*" && sid.Namespace != namespace {
+		return false
+	}
+
+	if collection != "*" && sid.Collection != collection {
+		return false
+	}
+
+	return true
+}
+
+func SemanticIDValidation(fl validator.FieldLevel) bool {
 	value := fl.Field().Interface()
-	collection := fl.Param()
 
-	switch value.(type) {
-	case *SemanticID:
-		v := value.(*SemanticID)
-		return v != nil && !v.IsNil() && v.Collection == collection
+	param := fl.Param()
+	namespace := "*"
+	collection := "*"
+	parts := strings.Split(param, Separator)
 
-	case SemanticID:
-		v := value.(SemanticID)
-		return !v.IsNil() && v.Collection == collection
-
-	case []SemanticID:
-		v := value.([]SemanticID)
-
-		for _, elem := range v {
-			if elem.IsNil() || elem.Collection != collection {
-				return false
-			}
+	//NOTE(happens): We panic here in case of an invalid param,
+	// similar to how the baked in validators handle this case
+	switch len(parts) {
+	case 2:
+		namespace = parts[0]
+		collection = parts[1]
+	case 1:
+		if parts[0] == "" {
+			panic("Expected an argument for sid validator")
 		}
 
-		return true
-	case []*SemanticID:
-		v := value.([]*SemanticID)
+		collection = parts[0]
+	default:
+		panic(fmt.Sprintf("Bad sid validation argument: %s", param))
+	}
 
-		for _, elem := range v {
-			if elem == nil || elem.IsNil() || elem.Collection != collection {
+	switch value.(type) {
+	case string:
+		sid, err := FromString(value.(string))
+		if err != nil {
+			return false
+		}
+
+		return validateSIDPrefix(sid, namespace, collection)
+	case []string:
+		for _, s := range value.([]string) {
+			sid, err := FromString(s)
+			if err != nil {
+				return false
+			}
+
+			if !validateSIDPrefix(sid, namespace, collection) {
 				return false
 			}
 		}
@@ -40,6 +70,60 @@ func SemanticIDCollectionValidation(fl validator.FieldLevel) bool {
 	return false
 }
 
+func SemanticIDTypeFunc(field reflect.Value) interface{} {
+	raw := field.Interface()
+	switch raw.(type) {
+	case SemanticID:
+		id := raw.(SemanticID)
+		if id.IsNil() {
+			return ""
+		}
+
+		return id.String()
+	case *SemanticID:
+		id := raw.(*SemanticID)
+		if id.IsNil() {
+			return ""
+		}
+
+		return id.String()
+	case []SemanticID:
+		ids := raw.([]SemanticID)
+		idStrs := make([]string, len(ids))
+		for i, id := range ids {
+			if id.IsNil() {
+				idStrs[i] = ""
+			} else {
+				idStrs[i] = id.String()
+			}
+		}
+
+		return idStrs
+	case []*SemanticID:
+		ids := raw.([]*SemanticID)
+		idStrs := make([]string, len(ids))
+		for i, id := range ids {
+			if id.IsNil() {
+				idStrs[i] = ""
+			} else {
+				idStrs[i] = id.String()
+			}
+		}
+
+		return idStrs
+	}
+
+	return ""
+}
+
 func RegisterValidation(v *validator.Validate) {
-	v.RegisterValidation("sidcol", SemanticIDCollectionValidation)
+	v.RegisterCustomTypeFunc(
+		SemanticIDTypeFunc,
+		SemanticID{},
+		&SemanticID{},
+		[]SemanticID{},
+		[]*SemanticID{},
+	)
+
+	v.RegisterValidation("sid", SemanticIDValidation)
 }
--- validator_test.go
+++ validator_test.go
@@ -1,53 +1,126 @@
-package semanticid
+package semanticid_test
 
 import (
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
 	"github.com/go-playground/validator/v10"
-	. "gopkg.in/check.v1"
+	"github.com/happenslol/semanticid"
 )
 
-var _ = Suite(&validatorTestSuite{})
-var val *validator.Validate
+type TestValidation struct {
+	Both                semanticid.SemanticID `validate:"sid=test.entities"`
+	OnlyNamespace       semanticid.SemanticID `validate:"sid=testname.*"`
+	OnlyCollection      semanticid.SemanticID `validate:"sid=*.testcol"`
+	OnlyCollectionShort semanticid.SemanticID `validate:"sid=short"`
+	Any                 semanticid.SemanticID `validate:"sid=*.*"`
+	Required            semanticid.SemanticID `validate:"required"`
 
-type TestCollectionValidation struct {
-	ID SemanticID `validate:"sidcol=entities"`
+	Pointer      *semanticid.SemanticID   `validate:"sid=testcol"`
+	Array        []semanticid.SemanticID  `validate:"sid=testcol"`
+	PointerArray []*semanticid.SemanticID `validate:"sid=testcol"`
 }
 
-type TestArrayCollectionValidation struct {
-	MultiIDs []SemanticID `validate:"sidcol=entities"`
+type NoArgValidation struct {
+	Test semanticid.SemanticID `validate:"sid"`
 }
 
-type TestPointerCollectionValidation struct {
-	PointerID *SemanticID `validate:"sidcol=entities"`
+type InvalidArgValidation struct {
+	Test semanticid.SemanticID `validate:"sid=1.2.3.4"`
 }
 
-type TestPointerArrayCollectionValidation struct {
-	MultiPointerIDs []SemanticID `validate:"sidcol=entities"`
-}
+var _ = Describe("validator", func() {
+	var (
+		validate   *validator.Validate
+		testStruct *TestValidation
+	)
 
-type validatorTestSuite struct{}
+	BeforeEach(func() {
+		validate = validator.New()
+		semanticid.RegisterValidation(validate)
 
-func TestValidator(t *testing.T) { TestingT(t) }
+		pointerID := semanticid.Must(semanticid.New("any", "testcol"))
 
-func (s *validatorTestSuite) SetUpSuite(c *C) {
-	val = validator.New()
-	RegisterValidation(val)
-}
+		testStruct = &TestValidation{
+			Both:                semanticid.Must(semanticid.New("test", "entities")),
+			OnlyNamespace:       semanticid.Must(semanticid.New("testname", "any")),
+			OnlyCollection:      semanticid.Must(semanticid.New("any", "testcol")),
+			OnlyCollectionShort: semanticid.Must(semanticid.New("any", "short")),
+			Any:                 semanticid.Must(semanticid.New("01234", "56789")),
+			Required:            semanticid.Must(semanticid.NewDefault()),
 
-func (s *validatorTestSuite) TestValidator(c *C) {
-	sid, err := NewWithCollection("entities")
-	c.Assert(err, IsNil)
+			Pointer: &pointerID,
+			Array: []semanticid.SemanticID{
+				semanticid.Must(semanticid.New("any", "testcol")),
+				semanticid.Must(semanticid.New("any", "testcol")),
+				semanticid.Must(semanticid.New("any", "testcol")),
+				semanticid.Must(semanticid.New("any", "testcol")),
+				semanticid.Must(semanticid.New("any", "testcol")),
+			},
+			PointerArray: []*semanticid.SemanticID{
+				&pointerID,
+				&pointerID,
+				&pointerID,
+				&pointerID,
+				&pointerID,
+			},
+		}
+	})
 
-	wrongCollectionSID, err := NewWithCollection("wrong")
-	c.Assert(err, IsNil)
+	Describe("Validating a struct with semanticid fields", func() {
+		Context("with valid values", func() {
+			It("should correctly validate", func() {
+				Expect(validate.Struct(testStruct)).To(BeNil())
+			})
+		})
 
-	collectionNil := TestCollectionValidation{}
-	c.Assert(val.Struct(collectionNil), NotNil)
+		Context("with invalid values", func() {
+			It("should correctly validate namespace and collection", func() {
+				testStruct.Both = semanticid.Must(semanticid.New("test", "invalid"))
+				Expect(validate.Struct(testStruct)).NotTo(BeNil())
 
-	collectionInvalid := TestCollectionValidation{ID: wrongCollectionSID}
-	c.Assert(val.Struct(collectionInvalid), NotNil)
+				testStruct.Both = semanticid.Must(semanticid.New("invalid", "entities"))
+				Expect(validate.Struct(testStruct)).NotTo(BeNil())
 
-	collectionValid := TestCollectionValidation{ID: sid}
-	c.Assert(val.Struct(collectionValid), IsNil)
-}
+				testStruct.Both = semanticid.Must(semanticid.New("invalid", "invalid"))
+				Expect(validate.Struct(testStruct)).NotTo(BeNil())
+			})
+
+			It("should correctly validate namespace only", func() {
+				testStruct.OnlyNamespace.Collection = "invalid"
+				Expect(validate.Struct(testStruct)).To(BeNil())
+
+				testStruct.OnlyNamespace.Namespace = "invalid"
+				Expect(validate.Struct(testStruct)).NotTo(BeNil())
+			})
+
+			It("should correctly validate collection only", func() {
+				testStruct.OnlyCollection.Namespace = "invalid"
+				Expect(validate.Struct(testStruct)).To(BeNil())
+
+				testStruct.OnlyCollection.Collection = "invalid"
+				Expect(validate.Struct(testStruct)).NotTo(BeNil())
+			})
+
+			It("should correctly validate collection with shorthand", func() {
+				testStruct.OnlyCollection.Namespace = "invalid"
+				Expect(validate.Struct(testStruct)).To(BeNil())
+
+				testStruct.OnlyCollection.Collection = "invalid"
+				Expect(validate.Struct(testStruct)).NotTo(BeNil())
+			})
+		})
+
+		Context("with incorrect validate tag arguments", func() {
+			It("should panic when no arguments are passed", func() {
+				fn := func() { validate.Struct(&NoArgValidation{}) }
+				Expect(fn).To(Panic())
+			})
+
+			It("should panic when an argument with more than 1 separator is passed", func() {
+				fn := func() { validate.Struct(&InvalidArgValidation{}) }
+				Expect(fn).To(Panic())
+			})
+		})
+	})
+})
